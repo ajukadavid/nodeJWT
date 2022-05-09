c5d33d1a41460857c4e80ad5ae774887edb06378
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-09 16:28:08 +0200

    generated token

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ app.get('/api', (req, res) => {
         message: 'Hey there! Welcome to this API service'
     })
 })
+app.post('/api/posts', (req, res) => {
+    res.json({
+        message: 'Posts created..'
+    })
+})
+app.post('/api/login', (req, res) => {
+    const user = {
+        id: 1,
+        username: 'John',
+        email: 'john@gmail.om'
+    }
+    jwt.sign({user: user}, 'secretKey',(err, token) => {
+        res.json({
+            token
+        })
+    })
+})
 
 app.listen(3000, (req, res) => {
-    console.log('server started on port 3000')
+    console.log('Server started on port 3000')
 })
